e0abb4d2a2fff859c9567b67832cb9ecb1d638e9
Eivind Uggedal <eu@redflavor.com>
2008-04-06 04:58:40 +0200

    New version.

--- lib/rubbr.rb
+++ lib/rubbr.rb
@@ -2,7 +2,7 @@ require 'optparse'
 $:.unshift File.dirname(__FILE__)
 
 module Rubbr
-  VERSION = '1.0.6'
+  VERSION = '1.1.0'
 
   autoload :Options,       'rubbr/options'
   autoload :Cli,           'rubbr/cli'
