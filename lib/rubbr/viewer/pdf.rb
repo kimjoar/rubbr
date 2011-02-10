6c499c1d08c1bbe397f09519b11d7d1d8402bc2c
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2011-02-10 13:36:50 +0100

    Added 'open' to view PDFs on OS X

--- lib/rubbr/viewer/pdf.rb
+++ lib/rubbr/viewer/pdf.rb
@@ -4,7 +4,7 @@ module Rubbr
       def initialize(*args)
         super
         @view_name = 'pdf'
-        @executables = %w(evince acroread xpdf gv)
+        @executables = %w(evince acroread xpdf gv open)
       end
     end
   end
